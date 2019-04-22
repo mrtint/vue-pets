9b8515d29bb27a063f6642dc297a1367a46a2d8e
남한산 [mrtint] <mrtint@nexon.co.kr>
2019-04-22 16:06:43 +0900

    vue 파일 분리, 이미지 preview 기능 개발

---
+++ src/components/VuePetForm.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Pet name:"
+        label-for="input-1"
+        description="Let me know your buddy's name"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.petName"
+          type="text"
+          required
+          placeholder="Enter pet name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Introduce your buddy:" label-for="input-2">
+        <b-form-input id="input-2" v-model="form.desc" required placeholder="she is very clever"></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Choose buddy's picture:" label-for="input-3">
+        <b-form-file
+          v-model="form.file"
+          :state="Boolean(form.file)"
+          placeholder="Choose a file..."
+          drop-placeholder="Drop file here..."
+          required
+          accept=".jpg, .png, .gif"
+          @change="previewImage"
+        ></b-form-file>
+        <div class="mt-3">Selected file: {{ form.file ? form.file.name : '' }}</div>
+        <div>
+          <b-img :src="previewImageData"></b-img>
+        </div>
+      </b-form-group>
+
+      <b-button type="submit" variant="info">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result" v-if="hide">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        petName: "",
+        file: "",
+        desc: ""
+      },
+      show: true,
+      previewImageData: null
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset
+      this.form.petName = "";
+      this.form.file = "";
+      this.form.desc = "";
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+    previewImage(evt) {
+      var input = event.target;
+
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = e => {
+          this.previewImageData = e.target.result;
+        };
+        reader.readAsDataURL(input.files[0]);
+      } else {
+          this.previewImageData = null;
+      }
+    }
+  }
+};
+</script>
---
+++ src/components/VuePetList.vue
@@ -0,0 +1,60 @@
+<template>
+  <b-container fluid>
+    <b-img v-for="img in list" :key="img.id" :src="img.src" :alt="img.petName" :title="img.desc" thumbnail fluid ></b-img>
+    <hr/>
+    <VuePetForm/>
+  </b-container>
+</template>
+
+<script>
+import VuePetForm from "./VuePetForm.vue";
+
+export default {
+  name: "pet-list",
+  components : {
+    VuePetForm
+  },
+  data: function() {
+    return {
+      greeting: "hello test!",
+      list: [
+        {
+          id: 1,
+          userName: "John doe",
+          petName: "HelloWorld",
+          src: "https://picsum.photos/250/250/?image=40",
+          desc: "he is my pet."
+        },
+        {
+          id: 2,
+          userName: "Jenny",
+          petName: "World",
+          src: "https://picsum.photos/250/250/?image=41",
+          desc: "she is my pet."
+        },
+        {
+          id: 3,
+          userName: "Andy",
+          petName: "Tommy",
+          src: "https://picsum.photos/250/250/?image=38",
+          desc: "there is no description"
+        },
+        {
+          id: 4,
+          userName: "Hansan",
+          petName: "Pen",
+          src: "https://picsum.photos/250/250/?image=22",
+          desc: "blank"
+        },
+        {
+          id: 5,
+          userName: "Tanaka",
+          petName: "Penguin",
+          src: "https://picsum.photos/250/250/?image=11",
+          desc: "petto"
+        }
+      ]
+    };
+  }
+};
+</script>
